c13ee84caaacc5e997521873cd0c09e9adb00b22
sgsarbere <sgsarbere@yahoo.com>
2022-06-06 22:56:07 +0530

    Add change for flippingBit

---
+++ Practice/FlippingBits.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    internal class FlippingBitsResult
+    {
+        /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+        public static long FlippingBits(long number)
+        {
+            //string number = "100";
+            //int fromBase = 10;
+            int toBase = 2;
+            string result = Convert.ToString(number, toBase);
+            Console.WriteLine("converted to binary: {0}", result);
+            int flipNo = 1;
+            long output = Convert.ToInt64(result, 2);
+            Console.WriteLine("initial string before xor: {0}", output);
+
+            Console.WriteLine(output);
+            for (int i = result.Length - 1; i >= result.Length - flipNo; --i)
+            { 
+                output ^= 1 << i;
+                Console.WriteLine("output: {0}", output);
+            }
+
+            Console.WriteLine(output);
+            return output;
+        }
+
+        public static string ConvertDecimalToBinary(int decimalNumber)
+        {
+            int remainder;
+            string result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+            return result;
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace Practice
 {
@@ -6,7 +18,26 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            //Console.WriteLine("Decimal:{0}, Binary: {1}",q, FlippingBitsResult.ConvertDecimalToBinary(q));
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+
+
+                long result = FlippingBitsResult.FlippingBits(n);
+
+                textWriter.WriteLine(result);
+            }
+
+            textWriter.Flush();
+            textWriter.Close();
+            Console.ReadLine();
         }
     }
 }
