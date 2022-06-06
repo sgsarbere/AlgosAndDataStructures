28884164660359917c38c07a11466e154c016011
sgsarbere <sgsarbere@yahoo.com>
2022-06-06 22:11:09 +0530

    adding gitignore files and a demo solution

---
+++ Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
